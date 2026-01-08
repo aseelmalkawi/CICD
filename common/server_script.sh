ae5e22713fbb30bafc07601d2c568a6574867e65
Aseel <malkawiaseel@live.com>
2026-01-08 05:04:07 -0500

    make scripts

--- common/server_script.sh
+++ common/server_script.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 
 echo "Logging into AWS ECR"
-aws ecr get-login-password --region ${{ secrets.AWS_REGION }} \
-| docker login --username AWS --password-stdin ${{ secrets.ECR_URI }}
+aws ecr get-login-password --region "$AWS_REGION" \
+| docker login --username AWS --password-stdin "$ECR_URI"
 
 echo "Pulling the Docker image"
-docker pull ${{ secrets.ECR_URI }}/cicd-aseel:${{ env.VERSION }}
+docker pull "$ECR_URI/cicd-aseel:$VERSION"
 
 if docker ps -a --format '{{.Names}}' | grep -wq nodeapp; then
-docker stop nodeapp
-docker rm nodeapp
+  docker stop nodeapp
+  docker rm nodeapp
 fi
 
 echo "Running new container"
-docker run -dp 3000:3000 --name nodeapp ${{ secrets.ECR_URI }}/cicd-aseel:${{ env.VERSION }}
+docker run -dp 3000:3000 --name nodeapp "$ECR_URI/cicd-aseel:$VERSION"
